224a30025bd06b6ea18736d0f9f1c4bf0cdb8c55
incosta1983 <isantoscosta@hotmail.com>
2025-07-28 18:14:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Esta linha é a mais importante para o deploy
+  base: '/sistema-escala-trabalho/', 
   plugins: [react()],
 })
